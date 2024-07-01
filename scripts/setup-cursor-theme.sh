fd14321976296ffd46399cf7cb4bde2d9aa97ab8
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-07-01 17:21:47 -0300

    chore: update configs

--- scripts/setup-cursor-theme.sh
+++ scripts/setup-cursor-theme.sh
@@ -3,7 +3,7 @@ CURSOR_NAME="Bibata-Modern-Ice"
 echo "Installing Bibata cursor..."
 
 # Set cursor
-gsettings set org.cinnamon.desktop.interface cursor-theme "$CURSOR_NAME"
+gsettings set org.cinnamon.desktop.interface cursor-theme $CURSOR_NAME
 gsettings set org.cinnamon.desktop.interface cursor-size 20
 
 echo "Bibata cursor installed"
