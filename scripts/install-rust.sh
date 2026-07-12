2f7cfee3d629546c678e835c4cce93d625678dda
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-07-11 21:00:06 -0300

    refactor: change npm and cargo packages

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+PWD=$(pwd)
+PACKAGES=$(tr '\n' ' ' <"${PWD}/cargo-packages")
 URL="https://sh.rustup.rs"
 
 echo "Installing rust..."
 curl --proto '=https' --tlsv1.2 -sSf "$URL" | sh -s -- -y --default-toolchain stable --no-modify-path
 source "$HOME/.cargo/env"
 rustup default stable
+
+echo "Installing cargo packages..."
+echo "$PACKAGES" | xargs cargo install
+echo "Cargo packages installed."
 
 echo "Rust installed."
